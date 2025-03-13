70d6fc83496f89ff07f78d415f3239c57fee0dc9
CDA <leonardonunes.link@gmail.com>
2025-03-13 09:37:56 -0300

    First: Neovim config unfinished

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
+    },
+}
